bf837417e1d3a2f28d404eb6b0eae0270d271345
Matt Moore <mattmoore@carbonhelix.com>
2020-01-30 21:30:04 -0600

    Add EitherT<ForIO, Exception, Address>.

--- src/main/kotlin/arrow/monad/transformer/example/App.kt
+++ src/main/kotlin/arrow/monad/transformer/example/App.kt
@@ -1,21 +1,35 @@
 package arrow.monad.transformer.example
 
-import arrow.core.Either
+import arrow.core.*
+import arrow.fx.ForIO
 import arrow.fx.IO
-import arrow.fx.extensions.fx
+import arrow.fx.extensions.io.monad.monad
 import arrow.mtl.EitherT
+import arrow.fx.fix
+import arrow.mtl.extensions.eithert.monad.monad
+import arrow.mtl.fix
 
-fun getAddressFromOrder(orderId: Int): IO<Address> = IO.fx {
-    val order = !getOrder(orderId)
-    val customer = !getCustomer(order.customerId)
-    val address = !getAddress(customer.addressId)
-    address
-}
+fun getAddressFromOrder(orderId: Int): EitherT<ForIO, Exception, Address> =
+        EitherT.monad<ForIO, Exception>(IO.monad()).fx.monad {
+            val order = !getOrder(orderId)
+            val customer = !getCustomer(order.customerId)
+            val address = !getAddress(customer.addressId)
+            address
+        }.fix()
 
 suspend fun main(args: Array<String>) {
-    getAddressFromOrder(1).attempt().map {
+    // With an invalid order number, we get the Left type containing the Exception and print the Exception message.
+    getAddressFromOrder(-1).value().fix().map {
         when (it) {
-            is Either.Left  -> println(it.a.message)
+            is Either.Left -> println(it.a.message)
+            is Either.Right -> println(it.b)
+        }
+    }.suspended()
+
+    // With an valid order number, we get the Right type containing the address that was found.
+    getAddressFromOrder(1).value().fix().map {
+        when (it) {
+            is Either.Left -> println(it.a.message)
             is Either.Right -> println(it.b)
         }
     }.suspended()
--- src/main/kotlin/arrow/monad/transformer/example/Data.kt
+++ src/main/kotlin/arrow/monad/transformer/example/Data.kt
@@ -1,27 +1,38 @@
 package arrow.monad.transformer.example
 
+import arrow.core.Left
+import arrow.core.Right
+import arrow.fx.ForIO
 import arrow.fx.IO
+import arrow.fx.handleError
+import arrow.mtl.EitherT
 
 val addresses = listOf(
-  Address(1, "123 Anywhere Street", "Chicago", "IL")
+        Address(1, "123 Anywhere Street", "Chicago", "IL")
 )
 
 val customers = listOf(
-  Customer(1, "Matt", "Moore", 1)
+        Customer(1, "Matt", "Moore", 1)
 )
 
 val orders = listOf(
-  Order(1, 1)
+        Order(1, 1)
 )
 
-fun getOrder(id: Int) = IO<Order> {
-  orders.find { it.id == id }!!
-}
+fun getOrder(id: Int): EitherT<ForIO, Exception, Order> = EitherT(
+        IO {
+            Right(orders.find { it.id == id }!!)
+        }.handleError { Left(Exception("Could not find order with ID $id.")) }
+)
 
-fun getCustomer(id: Int) = IO<Customer> {
-  customers.find { it.id == id }!!
-}
+fun getCustomer(id: Int): EitherT<ForIO, Exception, Customer> = EitherT(
+        IO {
+            Right(customers.find { it.id == id }!!)
+        }.handleError { Left(Exception("Could not find customer with ID $id.")) }
+)
 
-fun getAddress(id: Int) = IO<Address> {
-  addresses.find { it.id == id }!!
-}
+fun getAddress(id: Int): EitherT<ForIO, Exception, Address> = EitherT(
+        IO {
+            Right(addresses.find { it.id == id }!!)
+        }.handleError { Left(Exception("Could not find address with ID $id.")) }
+)
